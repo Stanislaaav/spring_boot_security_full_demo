cffd5b48a0616afbb97f1424ad828333565db5a7
Станислав Илиев <s.iliev@is-bg.net>
2021-07-19 10:24:57 +0300

    make some changes on config file

--- src/main/java/com/example/demo/config/ApplicationSecurityConfig.java
+++ src/main/java/com/example/demo/config/ApplicationSecurityConfig.java
@@ -1,21 +1,18 @@
 package com.example.demo.config;
 
-import static com.example.demo.enums.UserRoleEnum.ADMIN;
-import static com.example.demo.enums.UserRoleEnum.ADMINTRAINEE;
 import static com.example.demo.enums.UserRoleEnum.STUDENT;
 
+import com.example.demo.auth.ApplicationUserService;
 import java.util.concurrent.TimeUnit;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
@@ -24,9 +21,11 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final PasswordEncoder passwordEncoder;
+    private final ApplicationUserService applicationUserService;
 
-    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder, ApplicationUserService applicationUserService) {
         this.passwordEncoder = passwordEncoder;
+        this.applicationUserService = applicationUserService;
     }
 
     @Override
@@ -65,31 +64,15 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Override
-    @Bean
-    public UserDetailsService userDetailsServiceBean() throws Exception {
-
-        UserDetails ana = User.builder()
-            .username("ana")
-            .password(passwordEncoder.encode("pass"))
-//            .roles(STUDENT.name())
-            .authorities(STUDENT.getGrantedAutorities())
-            .build();
-
-        UserDetails linda = User.builder()
-            .username("linda")
-            .password(passwordEncoder.encode("pass1"))
-//            .roles(ADMIN.name())
-            .authorities(ADMIN.getGrantedAutorities())
-            .build();
-
-        UserDetails tom = User.builder()
-            .username("tom")
-            .password(passwordEncoder.encode("pass1"))
-//            .roles(ADMINTRAINEE.name())
-            .authorities(ADMINTRAINEE.getGrantedAutorities())
-            .build();
-
-        return new InMemoryUserDetailsManager(ana, linda, tom);
-
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider((daoAuthenticationProvider()));
     }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(passwordEncoder);
+        provider.setUserDetailsService(applicationUserService);
+        return provider;
+   }
 }
